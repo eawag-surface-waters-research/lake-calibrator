109a72208ceb9113bb149e094af4213662b8c733
James Runnalls <runnalls.james@gmail.com>
2024-08-26 11:33:06 +0200

    Live log to console

--- src/functions/general.py
+++ src/functions/general.py
@@ -2,16 +2,22 @@ import subprocess
 import pandas as pd
 from datetime import datetime, timezone
 
+
 def run_subprocess(command, debug=False):
-    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    if debug:
-        print(result.stdout)
-        print(result.stderr)
-    if result.returncode != 0:
-        error_message = f"Command failed with return code {result.returncode}\n"
+    process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    while True:
+        output = process.stdout.readline()
+        if output == '' and process.poll() is not None:
+            break
+        if output and debug:
+            print(output.strip())
+    stderr = process.communicate()[1]
+    if stderr and debug:
+        print(stderr.strip())
+    if process.returncode != 0:
+        error_message = f"Command failed with return code {process.returncode}\n"
         error_message += f"Command: {command}\n"
-        error_message += f"Standard Output: {result.stdout}\n"
-        error_message += f"Standard Error: {result.stderr}\n"
+        error_message += f"Standard Error: {stderr}\n"
         raise RuntimeError(error_message)
 
 def parse_observation_file(file, start, end):
