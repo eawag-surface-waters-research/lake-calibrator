Fix scipy error calculation

src/lake_calibrator/pest_calibrate.py
@@ -11,7 +11,7 @@ import pandas as pd
 from datetime import datetime
 from dateutil.relativedelta import relativedelta
 
-from .functions import run_subprocess, parse_observation_file, list_from_selection, datetime_from_days, days_since_year
+from .functions import run_subprocess, read_observation_data, list_from_selection, datetime_from_days, days_since_year
 from .simstrat import set_simstrat_outputs, simstrat_max_depth
 
 def pest_calibrate(args, log):
@@ -42,7 +42,8 @@ def pest_calibrate(args, log):
 
 def pest_input_files(args, log):
     log.info("Copying model input files", indent=1)
-    copy_model_inputs(args["calibration_folder"], args["simulation_folder"])
+    input_folder = os.path.join(args["calibration_folder"], "input")
+    copy_model_inputs(input_folder, args["simulation_folder"])
 
     log.info("Creating PEST .tpl file", indent=1)
     config = write_pest_tpl_file(args["calibration_folder"], args["simulation_folder"], args["parameters"], args["simulation"])
@@ -67,10 +68,10 @@ def pest_input_files(args, log):
 
     if args["simulation"] == "simstrat":
         if times[-1] + relativedelta(days=1) < end_date:
-            log.info("Editing PEST .tpl file to limit stop simulation at last observation", indent=1)
+            log.info("Editing PEST .tpl file to stop simulation at last observation", indent=1)
             overwrite_simstrat_tpl(args["calibration_folder"], times[-1], config["Simulation"]["Reference year"])
         log.info("Setting Simstrat output files", indent=1)
-        set_simstrat_outputs(args["calibration_folder"], times, depths, config["Simulation"]["Reference year"])
+        set_simstrat_outputs(input_folder, times, depths, config["Simulation"]["Reference year"])
 
     log.info("Creating PEST .ins files", indent=1)
     combined_observations = write_pest_ins_file(args["calibration_folder"], args["calibration_options"], args["simulation"], observations, times, depths)
@@ -83,8 +84,7 @@ def pest_input_files(args, log):
         observations_summary[obv["parameter"]] = {"times": len(set(obv["df"].index)), "depths": len(set(obv["df"]["depth"])), "total": len(obv["df"])}
     return observations_summary
 
-def copy_model_inputs(calibration_folder, simulation_folder):
-    output_folder = os.path.join(calibration_folder, "inputs")
+def copy_model_inputs(output_folder, simulation_folder):
     os.makedirs(output_folder)
     for item in os.listdir(simulation_folder):
         file = os.path.join(simulation_folder, item)
@@ -94,30 +94,6 @@ def copy_model_inputs(calibration_folder, simulation_folder):
             shutil.copy2(file, output_folder)
         elif item != "Results":
             shutil.copytree(file, os.path.join(output_folder, item))
-
-def read_observation_data(calibration_options, observations, start_date, end_date, max_depth):
-    times = []
-    depths = []
-    for objective_variable in calibration_options["objective_variables"]:
-        obs_ids = [i for i in range(len(observations)) if observations[i]["parameter"] == objective_variable]
-        if len(obs_ids) != 1:
-            raise ValueError("Cannot find {} observations to calculate residuals".format(objective_variable))
-        obs = observations[obs_ids[0]]
-        start = max(start_date, datetime.fromisoformat(obs["start"]))
-        end = min(end_date, datetime.fromisoformat(obs["end"]))
-        df = parse_observation_file(obs["file"], start, end, max_depth=max_depth)
-        times.extend(df.index.tolist())
-        depths.extend([d for d in df["depth"].tolist() if not np.isnan(d)])
-        observations[obs_ids[0]]["df"] = df
-    times = sorted(set(times))
-    depths = sorted(set(depths))
-    if len(depths) == 1:
-        print("WARNING: Only one depth in observations, only outputting a single depth can lead to unintended model outputs. Adding additional depth that is not calibrated on.")
-        if depths[0] == 0:
-            depths.append(1.0)
-        else:
-            depths.insert(0, depths[0]/2)
-    return times, depths, observations
 
 def write_pest_run_file(calibration_folder, docker_host_calibration_folder, execute, calibration_options):
     if "docker" in calibration_options and not calibration_options["docker"]:

src/lake_calibrator/scipy_calibrate.py
@@ -1,10 +1,12 @@
 import os
 import shutil
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
 from scipy.optimize import minimize
-from .simstrat import edit_par_file, copy_simstrat_inputs, simstrat_rms
-from .functions import run_subprocess
+from .simstrat import edit_par_file, copy_simstrat_inputs, simstrat_rms, simstrat_max_depth, set_simstrat_outputs
+from .functions import run_subprocess, read_observation_data, datetime_from_days, days_since_year
 
-iteration = 0
+iteration = 1
 run = 0
 
 def scipy_calibrate(args, log):
@@ -18,6 +20,24 @@ def scipy_calibrate(args, log):
         raise ValueError('Execute command in argument file should contain "Calibration.par" NOT the name of your par file.')
 
     if args["simulation"] == "simstrat":
+        base_folder = os.path.join(args["calibration_folder"], "base")
+        copy_simstrat_inputs(args["simulation_folder"], base_folder)
+        config = edit_par_file(base_folder, initial=True)
+        if "burn_in_days" in args["calibration_options"]:
+            log.info('Using burn in period of {} days'.format(args["calibration_options"]["burn_in_days"]), indent=2)
+            start_date = datetime_from_days(config["Simulation"]["Start d"], config["Simulation"]["Reference year"]) + relativedelta(days=args["calibration_options"]["burn_in_days"])
+        else:
+            log.info('"burn_in_days" not defined in calibration_options, using default of 365 days', indent=2)
+            start_date = datetime_from_days(config["Simulation"]["Start d"], config["Simulation"]["Reference year"]) + relativedelta(years=1)
+        end_date = datetime_from_days(config["Simulation"]["End d"], config["Simulation"]["Reference year"])
+        max_depth = simstrat_max_depth(args["simulation_folder"], config["Input"]["Morphology"])
+        times, depths, observations = read_observation_data(args["calibration_options"], args["observations"], start_date, end_date, max_depth)
+        if times[-1] + relativedelta(days=1) < end_date:
+            log.info("Editing PAR file to stop simulation at last observation", indent=1)
+            end_date = days_since_year(times[-1] + relativedelta(days=1), config["Simulation"]["Reference year"])
+            edit_par_file(base_folder, end_date=end_date)
+        log.info("Setting Simstrat output files", indent=1)
+        set_simstrat_outputs(base_folder, times, depths, config["Simulation"]["Reference year"])
         fun = simstrat304_iterator
     else:
         raise ValueError("Not implemented for {}".format(args["simulation"]))
@@ -61,16 +81,15 @@ def simstrat304_iterator(parameter_values, args, log):
     log.info("Running: [{}]".format(", ".join(map(str, parameter_values))), indent=2)
     global run
     folder = os.path.abspath(os.path.join(args["calibration_folder"], "{}_{}".format(iteration, run)))
-    copy_simstrat_inputs(args["simulation_folder"], folder)
+    shutil.copytree(os.path.join(args["calibration_folder"], "base"), folder)
     parameter_names = [parameter["name"] for parameter in args["parameters"]]
-    reference_year = edit_par_file(folder, parameter_names, parameter_values)
+    config = edit_par_file(folder, parameter_names=parameter_names, parameter_values=parameter_values)
+    reference_year = config["Simulation"]["Reference year"]
     run_subprocess(args["execute"].format(calibration_folder=folder), cwd=folder)
     calib = args["calibration_options"]
     if calib["objective_function"] == "rms":
         error = simstrat_rms(calib["objective_variables"],
                              calib["objective_weights"],
-                             calib["time_mode"],
-                             calib["depth_mode"],
                              args["observations"],
                              reference_year,
                              os.path.join(folder, "Results"))

src/lake_calibrator/simstrat.py
@@ -7,28 +7,32 @@ from scipy.interpolate import interp1d
 from .functions import parse_observation_file, days_since_year
 
 
-def edit_par_file(folder, parameter_names, parameter_values):
+def edit_par_file(folder, initial=False, parameter_names=[], parameter_values=[], end_date=False):
     par_files = [f for f in os.listdir(folder) if f.endswith(".par")]
     par_file = os.path.join(folder, par_files[0])
     with open(par_file) as f:
         data = json.load(f)
-    reference_year = data["Simulation"]["Reference year"]
 
-    data["Output"]["Depths"] = "z_out.dat"
-    data["Output"]["Times"] = "t_out.dat"
-    data["Output"]["All"] = False
+    if initial:
+        data["Output"]["Depths"] = "z_out.dat"
+        data["Output"]["Times"] = "t_out.dat"
+        data["Output"]["All"] = False
 
-    data["Simulation"]["DisplaySimulation"] = 0
-    data["Simulation"]["Continue from last snapshot"] = False
-    data["Simulation"]["Show progress bar"] = False
-    data["Simulation"]["Save text restart"] = False
-    data["Simulation"]["Use text restart"] = False
+        data["Simulation"]["DisplaySimulation"] = 0
+        data["Simulation"]["Continue from last snapshot"] = False
+        data["Simulation"]["Show progress bar"] = False
+        data["Simulation"]["Save text restart"] = False
+        data["Simulation"]["Use text restart"] = False
 
-    for index, parameter in enumerate(parameter_names):
-        data["ModelParameters"][parameter] = parameter_values[index]
+    if end_date:
+        data["Simulation"]["End d"] = end_date
+
+    if len(parameter_names) > 0 and len(parameter_names) == len(parameter_values):
+        for index, parameter in enumerate(parameter_names):
+            data["ModelParameters"][parameter] = parameter_values[index]
     with open(par_file, "w") as f:
         json.dump(data, f, indent=4)
-    return reference_year
+    return data
 
 
 def copy_simstrat_inputs(src, dst):
@@ -52,7 +56,7 @@ def copy_simstrat_inputs(src, dst):
         else:
             shutil.copy2(s, d)
 
-def simstrat_rms(objective_variables, objective_weights, time_mode, depth_mode, observations, reference_year, folder):
+def simstrat_rms(objective_variables, objective_weights, observations, reference_year, folder):
     residuals = 0
     weights = 0
     for i, objective_variable in enumerate(objective_variables):
@@ -63,28 +67,14 @@ def simstrat_rms(objective_variables, objective_weights, time_mode, depth_mode, 
             obs = obs[0]
             df_obs = parse_observation_file(obs["file"], obs["start"], obs["end"])
             df_sim = parse_output_file(os.path.join(folder, "T_out.dat"), reference_year)
-            for index, row in df_obs.iterrows():
-                if time_mode == "nearest":
-                    time_index = df_sim.index.get_indexer([index], method='nearest')[0]
-                else:
-                    raise ValueError("Please select time_mode from [nearest]")
-                sim_row = df_sim.iloc[time_index]
-                if depth_mode == "linear_interpolation":
-                    x = sim_row.index.to_numpy().astype(float) * -1
-                    y = sim_row.values.astype(float)
-                    d = float(row["depth"])
-                    if d < np.min(x) or d > np.max(x):
-                        continue
-                    idx = np.argmax(x == d)
-                    if x[idx] == d:
-                        sim_value = y[idx]
-                    else:
-                        f = interp1d(x, y, kind='linear')
-                        sim_value = f(d)
-                else:
-                    raise ValueError("Please select depth_mode from [linear_interpolation]")
-                residuals = residuals + (objective_weights[i] * row["weight"] * (row["value"] - sim_value) ** 2)
-                weights = weights + (objective_weights[i] * row["weight"])
+            df_sim = df_sim.reset_index().melt(id_vars='time', var_name='depth', value_name='value')
+            df_sim['depth'] = df_sim['depth'].astype(float) * -1
+            df = df_obs.merge(df_sim, on=['time', 'depth'], how='left', suffixes=('_obs', '_sim'))
+            df = df.dropna()
+            df["residuals"] = (objective_weights[i] * df["weight"] * (df["value_obs"] - df["value_sim"]) ** 2)
+            df["obj_weights"] = (objective_weights[i] * df["weight"])
+            residuals = residuals + df['residuals'].sum()
+            weights = weights + df['obj_weights'].sum()
         else:
             raise ValueError("Not implemented for objective variable {}".format(objective_variable))
     return (residuals / weights) ** 0.5
@@ -101,11 +91,11 @@ def parse_output_file(file, reference_year):
 def set_simstrat_outputs(calibration_folder, times, depths, reference_year):
     if len(depths) < 2:
         raise ValueError("There is a single output depth in file (probably because there are observations only at one depth). This will be misunderstood by Simstrat.")
-    with open(os.path.join(calibration_folder, "inputs", "z_out.dat"), 'w') as file:
+    with open(os.path.join(calibration_folder, "z_out.dat"), 'w') as file:
         file.write("output depths\n")
         for z in depths:
             file.write("%.2f\n" % -abs(z))
-    with open(os.path.join(calibration_folder, "inputs", "t_out.dat"), 'w') as file:
+    with open(os.path.join(calibration_folder, "t_out.dat"), 'w') as file:
         file.write("output times\n")
         for t in times:
             file.write("%.4f\n" % days_since_year(t, reference_year))